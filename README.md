b2fec77a4a4ba6906e21ebc4eb91d3ebd383369e
Matthias Geier <Matthias.Geier@gmail.com>
2022-06-10 20:36:05 +0200

    Don't use cargo-readme anymore

--- README.md
+++ README.md
@@ -4,12 +4,14 @@ Rust implementation of the spline type that's used in the Audio Scene
 Description Format (ASDF), see
 <https://AudioSceneDescriptionFormat.readthedocs.io/>.
 
+
 ## Requirements
 
 * Rust compiler, Cargo (<https://rustup.rs/>)
 
 The required Rust packages (a.k.a. "crates") are listed in the file
 `Cargo.toml`.
+
 
 ## Tests
 
@@ -19,19 +21,14 @@ cargo test --workspace
 
 There are further tests (using Python) in the `python/` directory.
 
+
 ## API Documentation
 
 Run `cargo doc --workspace` in the main directory to create the documentation.
 The generated HTML documentation can be accessed via
-[target/doc/asdfspline/index.html](index.html) and
-[target/doc/asdfspline_ffi/index.html](../asdfspline_ffi/index.html).
+`target/doc/asdfspline/index.html` and `target/doc/asdfspline_ffi/index.html`.
 
-## Updating `README.md`
 
-Using [cargo-readme](https://github.com/livioribeiro/cargo-readme) (`cargo install cargo-readme`):
+## License
 
-```
-cargo readme -o README.md
-```
-
-License: MIT OR Apache-2.0
+MIT OR Apache-2.0
--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,7 @@
-/*!
-Rust implementation of the spline type that's used in the Audio Scene
-Description Format (ASDF), see
-<https://AudioSceneDescriptionFormat.readthedocs.io/>.
+//! Rust implementation of the spline type that's used in the Audio Scene
+//! Description Format (ASDF), see
+//! <https://AudioSceneDescriptionFormat.readthedocs.io/>.
 
-# Requirements
-
-* Rust compiler, Cargo (<https://rustup.rs/>)
-
-The required Rust packages (a.k.a. "crates") are listed in the file
-`Cargo.toml`.
-
-# Tests
-
-```text
-cargo test --workspace
-```
-
-There are further tests (using Python) in the `python/` directory.
-
-# API Documentation
-
-Run `cargo doc --workspace` in the main directory to create the documentation.
-The generated HTML documentation can be accessed via
-[target/doc/asdfspline/index.html](index.html) and
-[target/doc/asdfspline_ffi/index.html](../asdfspline_ffi/index.html).
-
-# Updating `README.md`
-
-Using [cargo-readme](https://github.com/livioribeiro/cargo-readme) (`cargo install cargo-readme`):
-
-```text
-cargo readme -o README.md
-```
-*/
 #![deny(unsafe_code)] // NB: A lot of unsafe code is in the "ffi" sub-crate
 
 use std::ops::{Add, Div, DivAssign, Mul, Sub};
